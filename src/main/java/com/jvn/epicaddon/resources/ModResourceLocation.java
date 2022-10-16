d7023f3a0240dc9ff7511e61aeaf2bcb04c4ecae
dfdyz <2649111464@qq.com>
2022-10-16 21:26:34 +0800

    Revert "EpicAddon"
    This reverts commit 98771efee25d4048490c4f84bc7e1a8f34aa3ee6.

--- src/main/java/com/jvn/epicaddon/resources/ModResourceLocation.java
+++
@@ -1,8 +0,0 @@
-package com.jvn.epicaddon.resources;
-
-import com.jvn.epicaddon.EpicAddon;
-import net.minecraft.resources.ResourceLocation;
-
-public class ModResourceLocation {
-    public static final ResourceLocation TrailBlue = new ResourceLocation(EpicAddon.MODID,"textures/trail/blue.png");
-}
--- src/main/java/com/jvn/epicaddon/tools/GlobalVal.java
+++
@@ -1,5 +0,0 @@
-package com.jvn.epicaddon.tools;
-
-public class GlobalVal {
-    public static float ANG = -60f;
-}
