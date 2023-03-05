f70ef88c79df91c037a98fda666fd5fc29e23323
dfdyz <2649111464@qq.com>
2023-03-05 16:20:13 +0800

    update voice

--- src/main/java/com/jvn/epicaddon/resources/EpicAddonSounds.java
+++ src/main/java/com/jvn/epicaddon/resources/EpicAddonSounds.java
@@ -13,6 +13,12 @@ import yesman.epicfight.main.EpicFightMod;
 @Mod.EventBusSubscriber(modid= EpicAddon.MODID, bus= Mod.EventBusSubscriber.Bus.MOD)
 public class EpicAddonSounds {
     public static final SoundEvent GENSHIN_BOW = RegSound("weapon.genshin_bow");
+    //public static final SoundEvent Yoimiya_Combo1 = RegSound("character.yoimiya.genshin_bow");
+    //public static final SoundEvent Yoimiya_Combo2 = RegSound("character.yoimiya.genshin_bow");
+
+    public static final SoundEvent Yoimiya_Skill1 = RegSound("character.yoimiya.skill1");
+    public static final SoundEvent Yoimiya_Skill2 = RegSound("character.yoimiya.skill2");
+    public static final SoundEvent Yoimiya_Skill3 = RegSound("character.yoimiya.skill3");
 
     private static SoundEvent RegSound(String name) {
         ResourceLocation r = new ResourceLocation(EpicAddon.MODID, name);
@@ -20,10 +26,14 @@ public class EpicAddonSounds {
         return s;
     }
 
+
     @SubscribeEvent
     public static void onSoundRegistry(final RegistryEvent.Register<SoundEvent> event) {
         event.getRegistry().registerAll(
-                GENSHIN_BOW
+                GENSHIN_BOW,
+                Yoimiya_Skill1,
+                Yoimiya_Skill2,
+                Yoimiya_Skill3
         );
     }
 }
--- src/main/java/com/jvn/epicaddon/utils/GlobalVal.java
+++ src/main/java/com/jvn/epicaddon/utils/GlobalVal.java
@@ -1,5 +1,9 @@
 package com.jvn.epicaddon.utils;
 
+import java.util.Random;
+
 public class GlobalVal {
     public static float ANG = -60f;
+    public static Random random = new Random(1145141919810L);
+
 }
